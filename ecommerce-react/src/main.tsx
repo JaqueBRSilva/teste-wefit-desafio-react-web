a10b312e54f23beef2fadd03b4325ede1ded865c
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-04-03 15:39:14 -0300

    feat: add rotas

--- ecommerce-react/src/main.tsx
+++ ecommerce-react/src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from './components/Header/index.tsx';
+import { GlobalStyle } from './global-style.ts';
+import Cart from './pages/Cart/index.tsx';
+import Home from './pages/Home/index.tsx';
+import SuccessfulPurchase from './pages/SuccessfulPurchase/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <GlobalStyle />
+
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        {/* <Route path='/shopping-cart' element={<Cart />}/> */}
+        {/* <Route path='/purchase' element={<SuccessfulPurchase />}/> */}
+      </Routes>
+
+    </BrowserRouter>
+  </React.StrictMode>
 )
