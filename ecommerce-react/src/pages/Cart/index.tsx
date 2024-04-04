b34f71ac971e40392076c95c6f7b72ece543f2e9
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-04-04 13:49:29 -0300

    feat: inclusão das telas adicionas nas rotas

---
+++ ecommerce-react/src/pages/Cart/index.tsx
@@ -0,0 +1,22 @@
+const Cart = () => {
+
+  function onCalculateTotalValue() { }
+
+  function handleRemoveItem() { }
+
+  function handleFinalizeOrder() { }
+
+  return (
+    <>
+      <h2 style={{ color: "white" }}>
+        SHOPPING-CART
+      </h2>
+
+      <h3 style={{ color: "white" }}>
+        Em Construção
+      </h3>
+    </>
+  )
+}
+
+export default Cart
---
+++ ecommerce-react/src/pages/SuccessfulPurchase/index.tsx
@@ -0,0 +1,17 @@
+
+const SuccessfulPurchase = () => {
+    
+    return (
+        <>
+            <h2 style={{ color: "white" }}>
+                PURCHASE
+            </h2>
+
+            <h3 style={{ color: "white" }}>
+                Em Construção
+            </h3>
+        </>
+    )
+}
+
+export default SuccessfulPurchase
