f4e93131103519a7c865b951da270116867d8314
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-05-02 10:24:30 -0300

    feat: add função p/ formatação de preços

---
+++ ecommerce-react/src/utils/formatCurrency.tsx
@@ -0,0 +1,8 @@
+const formatCurrencyForBRL = (value: number) => {
+    return value.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+}
+
+export default formatCurrencyForBRL
